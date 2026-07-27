cdf573cd6a84f94d4f443cc89d764b3fc1939537
Harsh-Naruka <hnaruka91@gmail.com>
2026-07-27 23:38:02 +0530

    2216A

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,25 +1,79 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int main(){
-    int n=0,i=0;
-    cin>>n;
-    int a[n];
-    while(i<n){
-        cin>>a[i];
-        i++;
-    }
-    int count=1,check=1;
-    for(int i=1; i<n; i++){
-            if(a[i]>=a[i-1]){
-                count++;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(k + 1);
+        for (int i = 1; i <= k; i++)
+            cin >> a[i];
+
+        vector<queue<int>> level(k + 2);
+        vector<int> cnt(k + 2, 0);
+
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            level[x].push(i);
+            cnt[x]++;
+        }
+
+        vector<int> ans;
+
+        while (true) {
+            bool finished = true;
+            for (int i = 1; i <= k; i++) {
+                if (cnt[i] > 0) {
+                    finished = false;
+                    break;
+                }
             }
-            else{
-                check=max(check,count);
-                count=1;
+
+            if (finished) break;
+
+            bool moved = false;
+
+            for (int i = k; i >= 1; i--) {
+                if (cnt[i] == 0) continue;
+
+                if (i == k || cnt[i + 1] < a[i + 1]) {
+                    int id = level[i].front();
+                    level[i].pop();
+
+                    level[i + 1].push(id);
+
+                    cnt[i]--;
+                    cnt[i + 1]++;
+
+                    ans.push_back(id);
+                    moved = true;
+                    break;
+                }
             }
+
+            if (!moved) {
+                cout << -1 << '\n';
+                goto next_case;
+            }
+        }
+
+        cout << ans.size() << '\n';
+        for (int x : ans)
+            cout << x << ' ';
+        cout << '\n';
+
+        next_case:;
     }
-    check=max(check,count);
-    cout<<check;
+
     return 0;
 }
