abd91dcbb5f6a38cbadac262e69ffa3c6118fa55
Harsh-Naruka <hnaruka91@gmail.com>
2026-07-27 23:04:12 +0530

    580A

--- codeforces.cpp
+++ codeforces.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main{
-    int n=0;
-    
+    int n=0,i=0;
+    cin>>n;
+    int a[n];
+    while(i<n){
+        cin>>a[i];
+        i++;
+    }
+    int count=0,check=0;
+    while(i<n){
+        
+        if(a[i]<a[i+1])count++;
+        else{
+            check=count;
+        }
+        i++;
+    }
     return 0;
 }
