66a1ab9a239e3ca4110642b05bc2cde803867e8c
Harsh-Naruka <hnaruka91@gmail.com>
2026-07-27 23:25:35 +0530

    580A

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main{
+int main(){
     int n=0,i=0;
     cin>>n;
     int a[n];
@@ -9,14 +9,17 @@ int main{
         cin>>a[i];
         i++;
     }
-    int count=0,check=0;
-    while(i<n){
-        
-        if(a[i]<a[i+1])count++;
-        else{
-            check=count;
-        }
-        i++;
+    int count=1,check=1;
+    for(int i=1; i<n; i++){
+            if(a[i]>=a[i-1]){
+                count++;
+            }
+            else{
+                check=max(check,count);
+                count=1;
+            }
     }
+    check=max(check,count);
+    cout<<check;
     return 0;
 }
