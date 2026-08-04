85c674aeeacc0bb806c4e0dc4193f8a0c3288e59
Harsh-Naruka <hnaruka91@gmail.com>
2026-08-04 23:30:25 +0530

    405A

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int main(){
-    
+    int n=0;cin>>n;
+    vector<int> vec1;
+    for(int i=0;i<n;i++){
+        cin>>vec1[i];
+    }
+    sort(vec1.begin(),vec1.end());
+    for(int i=0;i<n;i++){
+        cout<<vec1[i]<<" ";
+    }
     return 0;
 }
