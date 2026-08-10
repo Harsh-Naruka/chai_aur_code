ffd5e505d1fc1fbeaad6eda3b1557a293be8580d
Harsh-Naruka <hnaruka91@gmail.com>
2026-08-10 22:53:39 +0530

    160A

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int main(){
     int n;cin>>n;
     vector<int> v(n);
-    sort(v.begin(),v.end());
+    int total=0;
     for(int i=0;i<n;i++){
-        
+        cin>>v[i];
+        total+=v[i];
     }
+    sort(v.rbegin(),v.rend());
+    int mine=0;
+    int count=0;
+
+    for(int i=0;i<n;i++){
+        mine+=v[i];
+        count++;
+        if(mine>total-mine){
+            break;
+        }
+    }
+    cout<<count<<endl;
 }
