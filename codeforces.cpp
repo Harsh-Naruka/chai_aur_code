eaed769054d8398dc97e742e8993bc6201422a70
Harsh-Naruka <hnaruka91@gmail.com>
2026-08-19 23:16:39 +0530

    ..

--- codeforces.cpp
+++ codeforces.cpp
@@ -9,19 +9,6 @@ int main() {
         int n, H, M;
         cin >> n >> H >> M;
 
-        int current = H * 60 + M;
-        int ans = 24 * 60;
-
-        for (int i = 0; i < n; i++) {
-            int h, m;
-            cin >> h >> m;
-
-            int alarm = h * 60 + m;
-            int diff = alarm - current;
-        }
-
-        cout << ans / 60 << " " << ans % 60 << endl;
-    }
 
     return 0;
 }
