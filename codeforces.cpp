a64495371d0b94cd17e95c782defbfe0ace969ba
Harsh-Naruka <hnaruka91@gmail.com>
2026-08-27 22:45:31 +0530

    change 4

--- codeforces.cpp
+++ codeforces.cpp
@@ -2,16 +2,7 @@
 #include <algorithm>
 
 using namespace std;
-
-int main() {
-    // Fast I/O
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n;
-    cin >> n;
-
-    // Option 3: Remove the second-to-last digit
+to-last digit
     
 
     return 0;
