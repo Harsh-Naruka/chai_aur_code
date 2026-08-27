9117a1ea95995a50d8f94fb9c1b068a7c0d3193b
Harsh-Naruka <hnaruka91@gmail.com>
2026-08-27 22:44:57 +0530

    ..

---
+++ codeforces.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    // Option 1: Keep original number
+    int option1 = n;
+    
+    // Option 2: Remove the last digit
+    int option2 = n / 10;
+    
+    // Option 3: Remove the second-to-last digit
+    int option3 = (n / 100) * 10 + (n % 10);
+
+    // Print the maximum of the three possibilities
+    cout << max({option1, option2, option3}) << "\n";
+
+    return 0;
+}
