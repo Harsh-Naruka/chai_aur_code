59a93300aed055282c8b3ca5bc607ba641368a7c
Harsh-Naruka <hnaruka91@gmail.com>
2026-08-27 22:45:15 +0530

    change in 3

--- codeforces.cpp
+++ codeforces.cpp
@@ -12,10 +12,7 @@ int main() {
     cin >> n;
 
     // Option 3: Remove the second-to-last digit
-    int option3 = (n / 100) * 10 + (n % 10);
-
-    // Print the maximum of the three possibilities
-    cout << max({option1, option2, option3}) << "\n";
+    
 
     return 0;
 }
