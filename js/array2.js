c3bb1ec1b6578c2be899140ae795cea62aeb6592
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-04 17:12:51 +0530

    Add two number

---
+++ js/array2.js
@@ -0,0 +1,9 @@
+function addtwo(num1,num2){
+    return num1+num2;
+}
+
+let num1=Number(prompt("Enter first Number"));
+let num2 =Number(prompt("Enter Second number"));
+
+console.log(addtwo(num1, num2));
+
